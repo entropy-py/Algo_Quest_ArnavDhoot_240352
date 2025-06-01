3247f416c702d6da6ca0ab14be5c5eddae6f4b53
entropy-py <arnavdhoot06@gmail.com>
2025-06-01 17:21:46 +0530

    Q1.cpp

--- week1/Q1.cpp
+++ week1/Q1.cpp
@@ -1,41 +1,29 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int t,n,j;
-    cin >> t;
-    while(t--){
-        int beauty=0,pos=0;
-    cin >> n;
-    vector<int> a(n), b(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++) cin >> b[i];
-     if(n==1){
-        cout<<"0"<<"\n";
-    }
-    else{
-    vector<pair<double, int>> c_with_index(n);
-    for (int i = 0; i < n; i++) {
-        c_with_index[i] = {double(a[i])/ b[i], i}; 
-    }
-    sort(c_with_index.begin(), c_with_index.end(),greater<>());
-    vector<int> sorted_b(n);
-    for (int i = 0; i < n; i++) {
-        sorted_b[i] = b[c_with_index[i].second];
-    };
-    vector<int> sorted_a(n);
-    for (int i = 0; i < n; i++) {
-        sorted_a[i] = a[c_with_index[i].second];
-    }
-    for(j=1;j<n;j++){
-
-        pos+=sorted_a[j-1];
-        beauty+=pos*sorted_b[j];
-    }
-    cout<<beauty<<"\n";
-    }
-    }
-    return 0;
+    int a,b,c,i,j,k,size,sum,x,count=0;
+cin>>size >>x;
+int arr[size];
+for(i=0;i<size;i++){
+    cin>>arr[i];
 }
+for(j=0;j<size;j++){
+    sum=arr[j];
+    if(sum==x){
+        count+=1;
+        continue;
+    }
+    else{for(k=j+1;k<size;k++){
+        sum=sum+arr[k];
+    if(sum==x){
+        count+=1;
+        break;
+    };
+    };
+    };
 
+}
+cout<<count;
+return 0;
+}
